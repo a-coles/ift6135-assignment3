fb6b507528fcd8cceb62ec8ddbaf6891089c0dfa
Arlie Coles <arlie.coles@mail.mcgill.ca>
2019-04-13 12:35:13 -0400

    Updating samplers

--- samplers.py
+++ samplers.py
@@ -2,22 +2,22 @@ import numpy as np
 import random
 
 
-def distribution1(x, batch_size=1):
+def distribution1(x, batch_size=512):
     # Distribution defined as (x, U(0,1)). Can be used for question 3
     while True:
         yield(np.array([(x, random.uniform(0, 1)) for _ in range(batch_size)]))
 
 
-def distribution2(batch_size=1):
+def distribution2(batch_size=512):
     # High dimension uniform distribution
     while True:
         yield(np.random.uniform(0, 1, (batch_size, 2)))
 
 
-def distribution3(batch_size=1):
-    # High dimension gaussian distribution
+def distribution3(batch_size=512):
+    # 1D gaussian distribution
     while True:
-        yield(np.random.normal(0, 1, (batch_size, 2)))
+        yield(np.random.normal(0, 1, (batch_size, 1)))
 
 e = lambda x: np.exp(x)
 tanh = lambda x: (e(x) - e(-x)) / (e(x)+e(-x))
@@ -25,7 +25,7 @@ def distribution4(batch_size=1):
     # arbitrary sampler
     f = lambda x: tanh(x*2+1) + x*0.75
     while True:
-        yield(f(np.random.normal(0, 1, (batch_size, 1))))
+    yield(f(np.random.normal(0, 1, (batch_size, 1))))
 
 
 
