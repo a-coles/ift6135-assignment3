ede775d183b95a41f1074001cfbf38f336fa1a6d
Jonathan Bhimani-Burrows <jonathanbhimani-burrows@hotmail.com>
2019-04-22 14:41:08 -0400

    Trying to fix the sampling

--- problem2/eval_methods.py
+++ problem2/eval_methods.py
@@ -5,7 +5,7 @@ This file contains evaluation methods for the VAE.
 import torch
 import torch.nn as nn
 import torch.distributions as dists
-from scipy.stats import norm
+# from scipy.stats import norm
 import numpy as np
 
 
@@ -29,7 +29,7 @@ def log_likelihood_estimate(model, loader, device, batch_size):
     '''
     Estimates the log likelihood by importance sampling.
     '''
-    k = 50
+    k = 200
     k_loss = []
     model.eval()
     # per batch
@@ -43,13 +43,18 @@ def log_likelihood_estimate(model, loader, device, batch_size):
             xi_l.extend([xi] * k)
             xi_m = torch.stack(xi_l)
             xi_m = xi_m.to(device)
+
             # calc input for loss
             output, mu, logvar = model(xi_m)
             stdev = torch.exp(0.5 * logvar)
-            #calco input for q
+
+            #calc input for q
             mu_ = mu[0,:]
             stdev_ = stdev[0,:]
+
             # generate and sample q
+            mu_ = mu_.cpu().detach()
+            stdev_ = stdev_.cpu().detach()
             q_gauss = dists.Normal(mu_, stdev_)
             q_samp = q_gauss.sample_n(k)
 
@@ -57,32 +62,22 @@ def log_likelihood_estimate(model, loader, device, batch_size):
             mu_0 = torch.zeros(100)
             sig_1 = torch.ones(100)
             p_gauss = dists.Normal(mu_0, sig_1)
-            p_samp = p_gauss.sample_n(k)
-            # send to device
-            q = norm.pdf(q_samp.cpu())
-            p = norm.pdf(p_samp.cpu())
-            p_t = torch.from_numpy(p)
-            q_t = torch.from_numpy(q)
-            q_t = q_t.to(device)
-            p_t = p_t.to(device)
-            # take sample
-            DKL = DKL_ksample(q_t, p_t, k)
+
+            # generate log prob
+            q = q_gauss.log_prob(q_samp)
+            p = p_gauss.log_prob(q_samp)
+            q = q.to(device)
+            p = p.to(device)
+
             # calc loss
             recon_loss = nn.BCELoss(reduction='sum')
             lo = recon_loss(output.float(), xi_m.float())
-            loss = lo + DKL
-            # loss = loss_fn(xi_m, output, batch_size=1, mu=mu, logvar=logvar)
-            # print('log sum exp loss is',loss)
-            k_loss.append(logSumExp(loss/k))
+            loss = lo/k + torch.exp(p) - torch.exp(q)
+            # print('lo is',lo)
+            # print('DKL is', DKL)
+            k_loss.append(logSumExp(loss))
     return k_loss
 
-
-def DKL_ksample(q, p, k):
-    # find a fast way to sum this
-    dkl = -1 * (q * torch.log(q) - q * torch.log(p))
-    # check axis
-    dkl = torch.mean(dkl)
-    return dkl
 
 def logSumExp(input):
     inp = input.cpu().detach().numpy()
